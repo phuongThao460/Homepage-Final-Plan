6aa0e1413be06ab596e4e01dc83f543d44c718df
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-05-16 22:17:15 +0700

    Update order

--- Controllers/Admin/OrderController.cs
+++ Controllers/Admin/OrderController.cs
@@ -19,5 +19,42 @@ namespace Homepage.Controllers
         {
             return View(db.DONHANGs.ToList());
         }
+        public ActionResult More(int id)
+        {
+            Session["Details"] = db.DONHANGs.Where(d => d.ID_DONHANG == id).FirstOrDefault();
+            return RedirectToRoute(new { controller = "Order", action = "DanhSachDonHang" });
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult SelectOrderState()
+        {
+            TRANGTHAIDONHANG ttnew = new TRANGTHAIDONHANG();
+            ttnew.lsTTDH = db.TRANGTHAIDONHANGs.ToList();
+            return PartialView(ttnew);
+        }
+        [HttpGet]
+        public PartialViewResult UpdateState()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public RedirectToRouteResult UpdateState(DONHANG newStateDH)
+        {
+            try
+            {
+                var check = db.DONHANGs.Where(dh => dh.ID_DONHANG == newStateDH.ID_DONHANG).FirstOrDefault();
+                check.ID_TRANGTHAI = newStateDH.ID_TRANGTHAI;
+                // Check if id == hoàn thành thì chuyển thành hóa đơn
+                db.SaveChanges();
+                return RedirectToAction("DanhSachDonHang");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("DanhSachDonHang");
+            }
+        }
     }
 }
