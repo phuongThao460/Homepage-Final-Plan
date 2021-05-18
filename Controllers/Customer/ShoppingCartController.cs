9c5f0b9fbe92339f638463153b74da2b3346f35c
phuongThao460 <blackcompany1998@gmail.com>
2021-05-18 17:33:17 +0700

    Initial commit

--- Controllers/Customer/ShoppingCartController.cs
+++ Controllers/Customer/ShoppingCartController.cs
@@ -64,5 +64,36 @@ namespace Homepage.Controllers
             ViewBag.QuantityCart = toltal_quantity_item;
             return PartialView("BagCart");
         }
+        public ActionResult CheckOut(FormCollection form)
+        {
+            try
+            {
+                Cart cart = Session["Cart"] as Cart;
+                DONHANG _order = new DONHANG();
+                _order.THOIGIAN_DAT =Convert.ToString(DateTime.Now);
+                _order.ID_TTKH = int.Parse(form["CodeCustomer"]);
+                database.DONHANGs.Add(_order);
+                foreach (var item in cart.Items)
+                {
+                    CHITIETDONHANG _order_detail = new CHITIETDONHANG();
+                    _order_detail.ID_DONHANG = _order.ID_DONHANG;
+                    _order_detail.ID_SACH = item._sach.ID_SACH;
+                    _order_detail.GIA_BAN = (double)item._sach.GIA_BAN;
+                    _order_detail.SOLUONG = (short)item._quantity;
+                    database.CHITIETDONHANGs.Add(_order_detail);
+                }
+                database.SaveChanges();
+                cart.ClearCart();
+                return RedirectToAction("CheckOut_Success", "ShoppingCart");
+            }
+            catch
+            {
+                return Content("Error checkout. Please check information of Customer... Thanks");
+            }
+        }
+        public ActionResult CheckOut_Success()
+        {
+            return View();
+        }
     }
 }
