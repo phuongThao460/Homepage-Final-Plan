89d1d0f2d782e09f468d431fcc4dcce360a08f43
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-06-09 19:29:14 +0700

    Update feedback
    [Bug] Can't get id of books

--- Controllers/Admin/FeedbackController.cs
+++ Controllers/Admin/FeedbackController.cs
@@ -10,15 +10,31 @@ namespace Homepage.Controllers.Admin
     {
         BookshopEntity db = new BookshopEntity();
         // GET: Feedback
+        [HttpGet]
         public ActionResult Index()
         {
-            return View(db.FEEDBACKs.ToList());
+            return View(db.SACHes.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(SACH sach)
+        {
+            return RedirectToRoute(new { controller = "Feedback", action = "SeeFullFeedback", id = sach.ID_SACH });
         }
         public ActionResult Delete(int id)
         {
             db.FEEDBACKs.Remove(db.FEEDBACKs.Where(f => f.ID_FEEDBACK == id).FirstOrDefault());
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public ActionResult SeeFullFeedback()
+        {
+            int id = int.Parse(Url.RequestContext.RouteData.Values["id"].ToString());
+            var ls = db.FEEDBACKs.Where(item => item.ID_SACH == id).ToList();
+            if(ls.Count <= 0)
+            {
+                ViewBag.State = null;
+            }
+            return View(ls);
         }
     }
 }
--- Models/FEEDBACK.cs
+++ Models/FEEDBACK.cs
@@ -30,5 +30,6 @@ namespace Homepage.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ANHFEEDBACK> ANHFEEDBACKs { get; set; }
         public virtual SACH SACH { get; set; }
+        public List<SACH> lsSach { get; set; }
     }
 }
